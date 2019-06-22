c1be2480783d0017c4ceef2397acd68153507413
Nathanael Wenzel <nate-wen.mc-soft@outlook.de>
2019-06-22 20:41:23 +0200

    change typo mistake

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -6,10 +6,10 @@ public class LightController : MonoBehaviour
 {
     public float sunSpeed = 10f;
 
-    private Vector3 rotationVecotr = new Vector3(0, 1, 0);
+    private Vector3 rotationVector = Vector3.up;
     // Update is called once per frame
     void Update()
     {
-        this.transform.RotateAround(Vector3.zero, rotationVecotr, sunSpeed * Time.deltaTime);
+        this.transform.RotateAround(Vector3.zero, rotationVector, sunSpeed * Time.deltaTime);
     }
 }
