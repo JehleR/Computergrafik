56684d038d5081b6acd7899f067d7d63f690bddc
Nathanael Wenzel <nate-wen.mc-soft@outlook.de>
2019-06-22 20:41:05 +0200

    changed flight simulator dragon is now not flying backwards

--- Assets/Scripts/FlyControl.cs
+++ Assets/Scripts/FlyControl.cs
@@ -4,40 +4,25 @@ using UnityEngine;
 
 public class FlyControl : MonoBehaviour
 {
-    float time = 0;
     float speed;
-    float width;
-    float height;
     float alpha;
     // Start is called before the first frame update
     void Start()
     {
         speed = 0.3f;
-        width = 400;
-        height = 400;
         alpha = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        
-        time += Time.deltaTime * speed;
-
-        float x = Mathf.Cos(time) * height;
-        float y = 220;
-        float z = Mathf.Sin(time) * width;
-
-        transform.position = new Vector3(x, y, z);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * 200);
 
         alpha = alpha + 0.001f;
-        if (alpha > 0.6)
+        if (alpha > 0.585)
         {
             alpha = 0;
         }
         transform.Rotate(0, -alpha, 0);
-        
-       
     }
 }
