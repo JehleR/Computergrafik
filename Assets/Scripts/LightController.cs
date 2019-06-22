25778eb2071832d6b70128ef7e59a836dd1926b2
Nathanael Wenzel <nate-wen.mc-soft@outlook.de>
2019-06-22 14:10:57 +0200

    sun controller for rotating

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public float sunSpeed = 10f;
+
+    private Vector3 rotationVecotr = new Vector3(0, 1, 0);
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.RotateAround(Vector3.zero, rotationVecotr, sunSpeed * Time.deltaTime);
+    }
+}
